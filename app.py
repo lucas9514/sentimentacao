60e22f8c4865f57521107509fd340c5c09beef05
Lucas Cavalcante <lucas.cavalcante@fhits.com.br>
2026-04-27 18:33:00 -0300

    remove matplotlib

--- app.py
+++ app.py
@@ -5,16 +5,7 @@ import textwrap
 from pathlib import Path
 from io import BytesIO
 from datetime import datetime
-
 import pandas as pd
-try:
-    import matplotlib.pyplot as plt
-except ModuleNotFoundError:
-    import subprocess
-    import sys
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "matplotlib==3.9.4"])
-    import matplotlib.pyplot as plt
-from matplotlib.patches import Circle, FancyBboxPatch
 import streamlit as st
 import requests
 from transformers import pipeline
